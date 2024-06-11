92499a0e5e8f4305bd4b4ec476a2bef71525bdc2
ayoubc03 <167438361+ayoubc03@users.noreply.github.com>
2024-06-11 14:14:44 +0200

    Daten werden weitergegeben, Try-Except Block

--- TCP (1515828)/conv.py
+++ TCP (1515828)/conv.py
@@ -3,13 +3,33 @@ import os
 import time
 
 def conv ():
+
+    
+
     while True:
-        log_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        stat_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        log_socket.connect(("localhost", 8888))  # Log-Prozess hört auf Port 8888
-        stat_socket.connect(("localhost", 8889))  # Stat-Prozess hört auf Port 8889
+        try: # Es wird erst versucht eine Verbindung herzustellen, erst dann wird eine Zahl generiert
 
-        while True:
-                messung = random.randint(0, 100)
-                print("Generierte Messung:", messung)
+            log_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            stat_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+            log_socket.connect(("localhost", 8888))  # Log-Prozess hört auf Port 8888
+            stat_socket.connect(("localhost", 8889))  # Stat-Prozess hört auf Port 8889
+
+            while True:
+                    messung = random.randint(0, 100) # Random Messwert wird generiert 
+                    print("Generierte Messung:", messung)
+
+                    log_socket.sendall(str(messung).encode()) # Senden an Log
+                    stat_socket.sendall(str(messung).encode()) # Senden an stat 
+                    time.sleep(1)   
+
+        
+        except (ConnectionRefusedError, BrokenPipeError) as e: # Exception falls Verbindung nicht klappt
+            print("Verbindung verloren, versuche erneut...", e)
+            time.sleep(1)
+        
+
+        finally:
+            log_socket.close() # Log Socket schließen für Ressourcenfreigabe und Stabilität
+            stat_socket.close() # Stat Socket schließen für Ressourcenfreigabe und Stabilität
