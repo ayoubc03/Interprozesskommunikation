9dcc842aa70bc37c540460ecba4ae3d857c67fcc
Adam-Ibra <adam.ibra2504@gmail.com>
2024-06-11 17:03:26 +0200

    Implementierung zufälliges Messwert u. Pipe 1

--- Pipes (1527289)/conv.py
+++ Pipes (1527289)/conv.py
@@ -1,4 +1,19 @@
 import random
 
-# Erzeugung der Zufallszahl(Messwert)
-messwert = random.randint(1,1000)
+fifo_path = '/tmp/myfifo'
+
+def generate_measurement(fifo_path):
+#Erzeugung der Zufallszahl(Messwert)
+ messwert = random.randint(1,1000)
+
+
+#Messwert in den Pipe schreiben lassen
+with open(fifo_path, 'w') as fifo:
+    fifo.write(str(messwert))
+    fifo.write('\n') 
+    
+    
+print(f"Messwert {messwert} wurde in den benannten Pipe '{fifo_path}' geschrieben.")   
+
+#Ausführen der Funktion
+generate_measurement()
