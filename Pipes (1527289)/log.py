e25d663af322d54b7ff56bd129fb403dc52d5203
Adam-Ibra <adam.ibra2504@gmail.com>
2024-06-13 12:53:01 +0200

    Fehler gefixt

--- Pipes (1527289)/log.py
+++ Pipes (1527289)/log.py
@@ -1,3 +1,4 @@
+import os
 
 fifo_path = '/tmp/myfifo'
 dateiname = 'messwerte.txt'
@@ -7,13 +8,15 @@ def messwert_in_datei(fifo_path, dateiname):
  with open(fifo_path, 'r') as fifo:
     messwert = fifo.read()
     
-    if not os.path.exist(dateiname)
+    if not os.path.exist(dateiname):
     #Messwert in eine Datei schreiben
-    with open(dateiname, 'w') as datei:
+     with open(dateiname, 'w') as datei:
             datei.write(messwert)
             print(f"Die Datei '{dateiname}' wurde erstellt und der Messwert {messwert} wurde in die Datei geschrieben.")
     else:
         print(f"Diese Datei existiert bereits.")
+        
+        
             
     
     
