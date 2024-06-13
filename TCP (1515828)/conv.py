e9fd2446ee9a0637489bdf0b10a06168f9489f82
ayoubc03 <167438361+ayoubc03@users.noreply.github.com>
2024-06-13 12:54:01 +0200

    ,

--- TCP (1515828)/conv.py
+++ TCP (1515828)/conv.py
@@ -7,21 +7,21 @@ def conv ():
 # Zwei Sockets werden hier erstellt um die Verbindung zu Log und Stat zu gewährleisten
     log_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   
-  #   stat_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    stat_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     while True:
 
         try: # Es wird erst versucht eine Verbindung herzustellen, erst dann wird eine Zahl generiert
 
             log_socket.connect(("localhost", 8888))  # Log-Prozess listened auf Port 8888
-     #       stat_socket.connect(("localhost", 8889))  # Stat-Prozess listened auf Port 8889
+            stat_socket.connect(("localhost", 8889))  # Stat-Prozess listened auf Port 8889
 
             while True:
                     messung = random.randint(0, 100) # Random Messwert wird generiert 
                     print("Generierte Messung:", messung)
 
                     log_socket.sendall(str(messung).encode()) # Senden an Log, Daten werden verschl+sselt
-                    # stat_socket.sendall(str(messung).encode()) # Senden an stat Daten werden verschl+sselt
+                    stat_socket.sendall(str(messung).encode()) # Senden an stat Daten werden verschl+sselt
                     time.sleep(1)  # Eine Sekunde warten zwischen den Verbindungsversuchen
 
         
@@ -32,7 +32,7 @@ def conv ():
         finally:
             try:
                 log_socket.close()  # Log Socket schließen für Ressourcenfreigabe und Stabilität
-     #           stat_socket.close()  # Stat Socket schließen für Ressourcenfreigabe und Stabilität
+                stat_socket.close()  # Stat Socket schließen für Ressourcenfreigabe und Stabilität
             except Exception as e:
                 print("Fehler beim Schließen der Sockets:", e)
 
