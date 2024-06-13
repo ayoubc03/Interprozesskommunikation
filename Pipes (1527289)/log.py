81ac9c12209d297b6568382c8e0c48fe7eea1a0b
Adam-Ibra <adam.ibra2504@gmail.com>
2024-06-13 12:45:03 +0200

    Ergänzung log.

--- Pipes (1527289)/log.py
+++ Pipes (1527289)/log.py
@@ -7,10 +7,14 @@ def messwert_in_datei(fifo_path, dateiname):
  with open(fifo_path, 'r') as fifo:
     messwert = fifo.read()
     
+    if not os.path.exist(dateiname)
     #Messwert in eine Datei schreiben
     with open(dateiname, 'w') as datei:
             datei.write(messwert)
             print(f"Die Datei '{dateiname}' wurde erstellt und der Messwert {messwert} wurde in die Datei geschrieben.")
+    else:
+        print(f"Diese Datei existiert bereits.")
+            
     
     
    
