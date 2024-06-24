074e2ffce2a6eb6c1429d41939fa9ae27050bd0e
Abuzar388 <zaher1497@gmail.com>
2024-06-24 12:04:01 +0200

    Hab report fertig gecodet

--- MessageQueues1508503)/report.py
+++ MessageQueues1508503)/report.py
@@ -5,3 +5,19 @@ import signal
 
 def report_process():
     report_queue = posix_ipc.MessageQueue("/report_queue", posix_ipc.O_CREAT)
+    def signal_handler(sig, frame):
+        print('Report-Prozess beendet.')
+        report_queue.close()
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, signal_handler)
+
+    while True:
+        try:
+            daten, _ = report_queue.receive()
+            daten = daten.decode()
+            summe, mittelwert = daten.split(',')
+            print(f"Report: Summe {summe}, Mittelwert {mittelwert}")
+            time.sleep(2)
+        except Exception as e:
+            print("Ein Fehler ist aufgetreten: ", e)
