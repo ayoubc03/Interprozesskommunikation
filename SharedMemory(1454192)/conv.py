eb036941ea1e72b96cea8b032d607142c24fef74
Yasinzaa <Yasin.zaatan@icloud.com>
2024-06-27 11:28:59 +0200

    Conv fertigestellt, random Zahl in SharedMemory geschrieben

--- SharedMemory(1454192)/conv.py
+++ SharedMemory(1454192)/conv.py
@@ -18,5 +18,36 @@ STAT_SEM_NAME = "/sem_stat"
 LOG_SHM_NAME = "/shm_log"
 LOG_SEM_NAME = "/sem_log"
 
+# Erstellen oder Öffnen von Shared Memory und Semaphoren
+stat_shm = posix_ipc.SharedMemory(STAT_SHM_NAME, posix_ipc.O_CREAT, size=1024)
+stat_sem = posix_ipc.Semaphore(STAT_SEM_NAME, posix_ipc.O_CREAT, initial_value=0)
+log_shm = posix_ipc.SharedMemory(LOG_SHM_NAME, posix_ipc.O_CREAT, size=1024)
+log_sem = posix_ipc.Semaphore(LOG_SEM_NAME, posix_ipc.O_CREAT, initial_value=0)
 
+# Erstellen von Memory Maps
+stat_speicher = mmap.mmap(stat_shm.fd, stat_shm.size)
+log_speicher = mmap.mmap(log_shm.fd, log_shm.size)
+
+# Schließen der Dateideskriptoren
+stat_shm.close_fd()
+log_shm.close_fd()
+
+# Endlosschleife zur Generierung von Daten
+while True:
+    wert = random.randint(0, 100)
+    daten = f"{wert}\n"
+
+    # Schreiben in das Log-Shared-Memory
+    log_speicher.seek(0)
+    log_speicher.write(daten.encode('utf-8'))
+    log_speicher.flush()
+    log_sem.release()
+
+    # Schreiben in das Stat-Shared-Memory
+    stat_speicher.seek(0)
+    stat_speicher.write(daten.encode('utf-8'))
+    stat_speicher.flush()
+    stat_sem.release()
+
+    time.sleep(1)
 
