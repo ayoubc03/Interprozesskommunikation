338b3075ee8fe9473b125f7bedc520ff44753806
Yasinzaa <Yasin.zaatan@icloud.com>
2024-06-28 16:44:24 +0200

    log fertiggestellt

--- SharedMemory(1454192)/log.py
+++ SharedMemory(1454192)/log.py
@@ -13,3 +13,22 @@ signal.signal(signal.SIGINT, sigint_handler)
 # Namen des Shared Memory und Semaphoren
 LOG_SHM_NAME = "/shm_log"
 LOG_SEM_NAME = "/sem_log"
+
+# Erstellen oder Öffnen von Shared Memory und Semaphoren
+log_shm = posix_ipc.SharedMemory(LOG_SHM_NAME)
+log_sem = posix_ipc.Semaphore(LOG_SEM_NAME)
+
+# Erstellen von Memory Maps
+log_speicher = mmap.mmap(log_shm.fd, log_shm.size)
+
+# Schließen des Dateideskriptors
+log_shm.close_fd()
+
+# Endlosschleife zum Schreiben in die Datei
+with open("log.txt", "w", encoding="utf-8") as logfile:
+    while True:
+        log_sem.acquire()
+        log_speicher.seek(0)
+        daten = log_speicher.read(1024).decode('utf-8').strip()
+        logfile.write(daten + "\n")
+        logfile.flush()
