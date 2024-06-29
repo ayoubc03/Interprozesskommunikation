130172fdb7ded443274ddc5c3f57b299a9fcc131
Yasinzaa <Yasin.zaatan@icloud.com>
2024-06-29 12:56:53 +0200

    erstellen von memory maps und semaphoren

--- SharedMemory(1454192)/stat.py
+++ SharedMemory(1454192)/stat.py
@@ -15,3 +15,17 @@ STAT_SHM_NAME = "/shm_stat"
 STAT_SEM_NAME = "/sem_stat"
 STAT_RESULT_SHM_NAME = "/shm_stat_result"
 STAT_RESULT_SEM_NAME = "/sem_stat_result"
+
+# Erstellen von Shared Memory und Semaphoren
+stat_shm = posix_ipc.SharedMemory(STAT_SHM_NAME)
+stat_sem = posix_ipc.Semaphore(STAT_SEM_NAME)
+stat_result_shm = posix_ipc.SharedMemory(STAT_RESULT_SHM_NAME, posix_ipc.O_CREAT, size=1024)
+stat_result_sem = posix_ipc.Semaphore(STAT_RESULT_SEM_NAME, posix_ipc.O_CREAT, initial_value=0)
+
+# Erstellen von Memory Maps
+stat_speicher = mmap.mmap(stat_shm.fd, stat_shm.size)
+stat_result_speicher = mmap.mmap(stat_result_shm.fd, stat_result_shm.size)
+
+# Schließen der Dateideskriptoren
+stat_shm.close_fd()
+stat_result_shm.close_fd()
