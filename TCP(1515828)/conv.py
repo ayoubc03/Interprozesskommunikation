e1cac4eca45e2ad113fa035d1591536bf4fc75c9
Yasinzaa <Yasin.zaatan@icloud.com>
2024-06-30 17:11:50 +0200

    fix

--- TCP(1515828)/conv.py
+++ TCP(1515828)/conv.py
@@ -33,8 +33,8 @@ def conv ():
                     messung = random.randint(0, 100) # Random Messwert wird generiert 
                     print("Generierte Messung:", messung)
 
-                    log_socket.sendall(str(messung).encode()) # Senden an Log, Daten werden verschl+sselt
-                    stat_socket.sendall(str(messung).encode()) # Senden an stat Daten werden verschl+sselt
+                    log_socket.sendall(str(messung).encode()) # Senden an Log, Daten werden in Bytes umgewandelt
+                    stat_socket.sendall(str(messung).encode()) # Senden an stat Daten in Bytes umgewandelt
                     time.sleep(1)  # Eine Sekunde warten zwischen den Verbindungsversuchen
 
         
